619fe8425814b631c5e1dc5b79060d65a9e85d4b
valtzman <xmodgamecenterhon@gmail.com>
2025-03-26 21:37:26 +0500

    first commit

---
+++ BACK/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/skip2/go-qrcode"
+	"log"
+	"net/http"
+)
+
+func generateQRCode(w http.ResponseWriter, r *http.Request) {
+	// Text to encode in QR Code
+	code := "google.com"
+
+	// Generate the QR code
+	qrCode, err := qrcode.Encode(code, qrcode.Medium, 256)
+	if err != nil {
+		log.Println("Error generating QR code:", err)
+		http.Error(w, "Failed to generate QR code", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+
+	w.Write(qrCode)
+}
+
+func main() {
+	http.HandleFunc("/generate-qrcode", generateQRCode)
+
+	// Start the server on port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
